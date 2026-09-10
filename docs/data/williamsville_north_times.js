b29ea9e556bf5632741457dd558d1140adc72be5
joefor <joe.forsyth@gmail.com>
2026-09-09 22:22:48 -0400

    added Williamsville North Varsity Letter times

---
+++ docs/data/williamsville_north_times.js
@@ -0,0 +1,10 @@
+const williamsvilleNorthTimes = [
+  { eventgender: "F", eventdistance: 200, eventstroke: "1", eventcourse: "Y", eventage: "1314", eventtime: "2:17.45" },
+  { eventgender: "F", eventdistance: 200, eventstroke: "5", eventcourse: "Y", eventage: "1314", eventtime: "2:37.83" },
+  { eventgender: "F", eventdistance: 50, eventstroke: "1", eventcourse: "Y", eventage: "1314", eventtime: "27.96" },
+  { eventgender: "F", eventdistance: 100, eventstroke: "4", eventcourse: "Y", eventage: "1314", eventtime: "1:15.25" },
+  { eventgender: "F", eventdistance: 100, eventstroke: "1", eventcourse: "Y", eventage: "1314", eventtime: "1:02.22" },
+  { eventgender: "F", eventdistance: 500, eventstroke: "1", eventcourse: "Y", eventage: "1314", eventtime: "6:22.68" },
+  { eventgender: "F", eventdistance: 100, eventstroke: "2", eventcourse: "Y", eventage: "1314", eventtime: "1:14.14" },
+  { eventgender: "F", eventdistance: 100, eventstroke: "3", eventcourse: "Y", eventage: "1314", eventtime: "1:26.22" },
+];
